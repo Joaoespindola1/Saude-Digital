9b581c668ab863c291a7718cae603ae9396db117
Gabriel Neves <126576810+YukiTsuki12@users.noreply.github.com>
2024-09-30 12:25:38 -0300

    Update models.py
    adicionando models para o postgres

--- saude_digital/app_saude_digital/models.py
+++ saude_digital/app_saude_digital/models.py
@@ -1,11 +1,45 @@
 from django.db import models
 
 class Cliente(models.Model):
-    id = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=255)
-    idade = models.IntegerField(null=True)
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, unique=True)
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(max_length=255)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
 
 class Corretor(models.Model):
-    id = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=255)
-    codigo = models.IntegerField(null=True)
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, unique=True)
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(max_length=255)
+    registro_susep = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class PlanoSaude(models.Model):
+    nome_plano = models.CharField(max_length=255)
+    tipo_plano = models.CharField(max_length=50, blank=True, null=True)
+    cobertura = models.TextField(blank=True, null=True)
+    valor_mensal = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome_plano
+
+class ClienteCorretor(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    corretor = models.ForeignKey(Corretor, on_delete=models.CASCADE)
+    plano = models.ForeignKey(PlanoSaude, on_delete=models.CASCADE)
+    data_associacao = models.DateField()
+    status_associacao = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        unique_together = (('cliente', 'corretor'),)
+
+    def __str__(self):
+        return f"{self.cliente.nome} - {self.corretor.nome}"
