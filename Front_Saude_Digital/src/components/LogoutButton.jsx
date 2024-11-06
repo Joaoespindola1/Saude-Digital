c7463b769ffd8b720b1a35ecf477a07a3675e1c4
samuelspalla1 <samuelspalla7@gmail.com>
2024-11-06 13:54:47 -0300

    new btn logout

--- Front_Saude_Digital/src/components/LogoutButton.jsx
+++ Front_Saude_Digital/src/components/LogoutButton.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { LogOut } from 'lucide-react'
+import { useNavigate } from "react-router-dom"
+
 
 const LogoutButton = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleLogout = () => {
-    localStorage.clear();
-    navigate("/login");
+    localStorage.clear()
+    navigate("/login")
   };
 
   return (
     <button
       onClick={handleLogout}
-      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      className="bg-white hover:bg-red-700 text-black border-2 border-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-200 ease-in-out"
     >
-      Desconectar
+      <LogOut />
     </button>
-  );
-};
+  )
+}
 
-export default LogoutButton;
+export default LogoutButton
